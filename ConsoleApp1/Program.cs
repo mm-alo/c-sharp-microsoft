5d7c977a2713c3c1afa23b37b50b6667eafacb9a
Emmanuel Alo <emmanuelalo209@gmail.com>
2026-08-11 14:50:38 +0800

    finished first module

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -95,3 +95,60 @@
 // Console.WriteLine("Nicolas: \t" + nicolasResults + " \tB");
 // Console.WriteLine("zahirah: \t" + zahirahResults + " \tB");
 // Console.WriteLine("jeong: \t\t" + jeongResults + " \tB");
+
+
+//gpa calculator
+//Divide the resulting sum by the total number of credit hours.
+// string studentName = "Sophia Johnson";
+// string course1Name = "English 101";
+// string course2Name = "Algebra 101";
+// string course3Name = "Biology 101";
+// string course4Name = "Computer Science I";
+// string course5Name = "Psychology 101";
+
+// int course1Credit = 3;
+// int course2Credit = 3;
+// int course3Credit = 4;
+// int course4Credit = 4;
+// int course5Credit = 3;
+
+// int gradeA = 4;
+// int gradeB = 3;
+
+// int course1Grade = gradeA;
+// int course2Grade = gradeB;
+// int course3Grade = gradeB;
+// int course4Grade = gradeB;
+// int course5Grade = gradeA;
+
+// int totalCreditHours = 0;
+// totalCreditHours += course1Credit;
+// totalCreditHours += course2Credit;
+// totalCreditHours += course3Credit;
+// totalCreditHours += course4Credit;
+// totalCreditHours += course5Credit;
+
+// int totalGradePoints = 0;
+// totalGradePoints += course1Credit * course1Grade;
+// totalGradePoints += course2Credit * course2Grade;
+// totalGradePoints += course3Credit * course3Grade;
+// totalGradePoints += course4Credit * course4Grade;
+// totalGradePoints += course5Credit * course5Grade;
+
+// decimal gradePointAverage = (decimal) totalGradePoints /totalCreditHours;
+// int leadingDigit = (int) gradePointAverage;
+
+
+// Console.WriteLine($"Student: {studentName} \n");
+// Console.WriteLine("Course \t \t\tGrade \tCredit Hours");
+// Console.WriteLine($"{course1Name}  \t\t{gradeA} \t\t{course1Credit}");
+// Console.WriteLine($"{course2Name} \t\t{gradeB} \t\t{course2Credit}");
+// Console.WriteLine($"{course3Name} \t\t{gradeB} \t\t{course3Credit}");
+// Console.WriteLine($"{course4Name} \t{gradeB} \t\t{course4Credit}");
+// Console.WriteLine($"{course5Name} \t\t{gradeA} \t\t{course5Credit}");
+// Console.WriteLine($"Final GPA: {gradePointAverage}");
+
+Random roll = new Random();
+int dice = roll.Next(-100, 45);
+
+Console.WriteLine(dice);
--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b874257a1045699e311d3421180506cdf96b44c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0713c8e7e2f0b414c0c238814ca52c86f18ebe3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
