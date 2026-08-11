0713c8e7e2f0b414c0c238814ca52c86f18ebe3f
Emmanuel Alo <emmanuelalo209@gmail.com>
2026-08-11 11:54:22 +0800

    module assesment - simple student grades

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e9a76b93cd0ac87377f074d1861de80b8ee4536")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b874257a1045699e311d3421180506cdf96b44c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
