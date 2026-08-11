4e9a76b93cd0ac87377f074d1861de80b8ee4536
Emmanuel Alo <emmanuelalo209@gmail.com>
2026-08-11 09:57:30 +0800

    module assesment forstring interpolation and verbatim string literal

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,8 +15,30 @@
 //escape sequences
 // Console.WriteLine("Hello \"World\" !");
 
-Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
-Console.WriteLine("Invoice: 1021 \t Complete!");
-Console.WriteLine("Invoice: 1022 \t Complete! \n");
-Console.WriteLine("Output Directory: ");
+// Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+// Console.WriteLine("Invoice: 1021 \t Complete!");
+// Console.WriteLine("Invoice: 1022 \t Complete! \n");
+// Console.Write("Output Directory: \t");
 
+//verbatim string literals
+// Console.Write(@" c\invoices");
+
+//string concatenation
+// string firstName = "Bob";
+// string message = "Hello" + firstName;
+// Console.WriteLine("\n\n"+message);
+
+//string interpolation
+// string firstName = "bob";
+// string message = $"hello {firstName}";
+// Console.WriteLine(message);
+
+//challenge for string interpolation and verbatim string literals
+
+string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine("View English output:");
+Console.WriteLine($@"  c:\Exercise\{projectName}\data.txt");
+Console.WriteLine("\n"+ russianMessage + ":");
+Console.WriteLine($"c:\\Exercise\\{projectName}\ru-Ru\\data.txt");
--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0553daf98221a1ac234da31bb8ad4874ed18d4fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8efffa38dd17ffaad71202cb529b5552ceca32b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
