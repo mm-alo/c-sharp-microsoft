188260793b0bbd3312e935a807cd2b412944a7b1
Emmanuel Alo <emmanuelalo209@gmail.com>
2026-08-13 11:15:34 +0800

    initializing and argeting an array through index

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -195,31 +195,44 @@
 
 // module challenge - if else block
 
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0; // will continue tommorow haha!
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0; // will continue tommorow haha!
 
-if (daysUntilExpiration == 0)
-{
-    Console.WriteLine("Your subscription has expired.");
-}
-else if (daysUntilExpiration == 1)
-{
-    Console.WriteLine("Your subscription expires within a day!");
-    discountPercentage = 20;
-}
-else if (daysUntilExpiration <= 5)
-{
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-    discountPercentage = 10;
-}
-else if (daysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-if (discountPercentage > 0)
-{
-    Console.WriteLine($"Renew now and save {discountPercentage}%.");
-}
+// if (daysUntilExpiration == 0)
+// {
+//     Console.WriteLine("Your subscription has expired.");
+// }
+// else if (daysUntilExpiration == 1)
+// {
+//     Console.WriteLine("Your subscription expires within a day!");
+//     discountPercentage = 20;
+// }
+// else if (daysUntilExpiration <= 5)
+// {
+//     Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+//     discountPercentage = 10;
+// }
+// else if (daysUntilExpiration <= 10)
+// {
+//     Console.WriteLine("Your subscription will expire soon. Renew now!");
+// }
+// if (discountPercentage > 0)
+// {
+//     Console.WriteLine($"Renew now and save {discountPercentage}%.");
+// }
+// arrays - big mind
 
-// next is arrays
+// string[] fraudulentOrderIDs = new string[3];
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+
+string[] fraudulentOrderIDs = [ "A123lo", "B456", "C789"];
+
+Console.WriteLine($"first: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+fraudulentOrderIDs[0] = "dragunuv";
+Console.WriteLine($"first: {fraudulentOrderIDs[0]} \n");
+Console.WriteLine($"how many orders do i have: {fraudulentOrderIDs.Length}");
