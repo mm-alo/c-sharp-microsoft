4499559394793423975dc481b75b056e7a165a5e
Emmanuel Alo <emmanuelalo209@gmail.com>
2026-08-20 10:41:52 +0800

    not yet done, can't even try to think

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6791af393270e5ad4e13bcc75aea775c792d42f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e206f8b14ba9adeadf6947712c6c52e0f21db68e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
